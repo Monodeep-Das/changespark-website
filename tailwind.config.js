536bbb651f4ebb10487ac5d343e8a04bd7aecebd
Monodeep Das <80527009+Monodeep-Das@users.noreply.github.com>
2024-07-14 14:42:18 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,16 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        "apercu": ["Apercu", "sans-serif"],
+      },
+      colors: {
+        'beige': '#FFFBEA',
+        'purp': '#6C4A7F',
+        'gren': '#27704E',
+      },
     },
   },
   plugins: [],
 };
+ 
