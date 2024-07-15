5dc4a8a0226c9b79578d0895aa93dcff22e02c90
Monodeep Das <80527009+Monodeep-Das@users.noreply.github.com>
2024-07-16 00:26:09 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,64 +1,40 @@
 import Image from "next/image";
+import Nav from "@/components/Nav";
+import AboutUs from "@/components/Aboutus";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-<main className="min-h-screen">
-  <nav className="flex items-center justify-between bg-beige h-20 pr-20 font-sans">
-    <div className="font-bold text-xl flex flex-row items-center">
-    <Image src="/logo2.png" alt="ChangeSpark Foundation logo" width={100} height={100} className="h-min w-40 pl-5 bg-transparent"/>
-    <h1 className="text-purp">ChangeSpark<span className="text-gren"> Foundation</span></h1></div>
-    <ul className="flex space-x-20">
-      <li className="list-none">
-        <a
-          href="/"
-          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
-        >
-          HOME
-        </a>
-      </li>
-      <li className="list-none">
-        <a
-          href="#about"
-          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
-        >
-          ABOUT US
-        </a>
-      </li>
-      <li className="list-none">
-        <a
-          href="#team"
-          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
-        >
-          TEAM
-        </a>
-      </li>
-      <li className="list-none">
-        <a
-          href="#projects"
-          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
-        >
-          PROJECTS
-        </a>
-      </li>
-      <li className="list-none">
-        <a
-          href="#join-us"
-          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
-        >
-          JOIN US
-        </a>
-      </li>
-      <li className="list-none">
-        <a
-          href="#donate"
-          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
-        >
-          DONATE
-        </a>
-      </li>
-    </ul>
-  </nav>
-</main>
+    <main className="flex flex-col min-h-screen">
+      <Nav  />
 
+      <div className="flex flex-col md:flex-row items-stretch p-8  bg-white h-screen">
+        <div className="md:flex-shrink-0 mb-8 md:mb-0 md:mr-8 flex flex-col justify-center">
+          <h1 className="text-4xl font-bold text-purple-900 dark:font-semibold  pt-12  mb-4 md:pl-24">
+            Changespark Foundation
+          </h1>
+          <p className="text-xl text-green-700 dark:font-semibold md:pl-24">
+            EMPOWERING POWER IGNITING CHANGE
+          </p>
+        </div>
+        <div className="md:flex-grow flex items-center justify-center">
+          <div className="aspect-w-16 aspect-h-9 w-full max-w-3xl">
+            <iframe className="md:w-[800px] md:h-112 h-100 w-[90vw]"
+              width="800"
+              height="450"
+              src="https://www.youtube.com/embed/Sqqj_14wBxU?si=gvouxtwpb9pqEbyc"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+        <AboutUs />
+        <Footer />
+      
+    </main>
   );
 }
