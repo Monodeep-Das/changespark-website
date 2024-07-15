af2bf7ce16e35375d0c0e396d96ff54981a581f7
Monodeep Das <80527009+Monodeep-Das@users.noreply.github.com>
2024-07-16 00:33:57 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,24 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      fontFamily: {
-        "apercu": ["Apercu", "sans-serif"],
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
+        green1: {
+          light: "#5EA384",
+          dark: "#0E6E44", 
+        },
+        purple1: {
+          light: "#8E68A5",
+          dark: "#623B7A", 
+        },
+        blue: "#1A3641",
+        lavender: "#FFF0F5",
+        rose : "#FFE4E1",
+        navyblue : "#4A0E4E",
         'beige': '#FFFBEA',
-        'purp': '#6C4A7F',
-        'gren': '#27704E',
       },
     },
   },
   plugins: [],
 };
- 
